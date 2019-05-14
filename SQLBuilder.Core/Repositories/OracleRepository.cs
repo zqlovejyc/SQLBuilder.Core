c4d81c844d1f615b27d861f360cabbf1d3269b7a
zqlovejyc <943620963@qq.com>
2019-05-14 09:54:07 +0800

    更新注释；

--- SQLBuilder.Core/Repositories/OracleRepository.cs
+++ SQLBuilder.Core/Repositories/OracleRepository.cs
@@ -70,7 +70,7 @@ namespace SQLBuilder.Core.Repositories
                     if (_dbConnection.State != ConnectionState.Open)
                         _dbConnection.Open();
                 }
-                //DbConnection被using后连接字符串被置为空
+                //判断DbConnection被using后连接字符串是否被置为空
                 else if (_dbConnection.ConnectionString.IsNullOrEmpty())
                 {
                     _dbConnection.ConnectionString = ConnectionString;

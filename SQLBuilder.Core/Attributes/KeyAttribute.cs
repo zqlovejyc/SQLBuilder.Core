84a7686d08ca460da9a57209f130bc4330fdcb7b
zqlovejyc <943620963@qq.com>
2021-01-01 10:11:09 +0800

    修改版权日期；

--- SQLBuilder.Core/Attributes/KeyAttribute.cs
+++ SQLBuilder.Core/Attributes/KeyAttribute.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 /***
- * Copyright © 2018-2020, 张强 (943620963@qq.com).
+ * Copyright © 2018-2021, 张强 (943620963@qq.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

bada8bcb00c4d87be63893fafaf0bf4b78fbff62
zqlovejyc <943620963@qq.com>
2021-01-20 15:49:39 +0800

    修复ExecuteTrans回滚rollback委托为null时异常；

--- SQLBuilder.Core/Repositories/BaseRepository.cs
+++ SQLBuilder.Core/Repositories/BaseRepository.cs
@@ -175,7 +175,9 @@ namespace SQLBuilder.Core.Repositories
                     else
                     {
                         repository.Rollback();
-                        rollback?.Invoke(null);
+
+                        if (rollback != null)
+                            rollback(null);
                     }
 
                     return res;
@@ -241,7 +243,9 @@ namespace SQLBuilder.Core.Repositories
                     else
                     {
                         repository.Rollback();
-                        await rollback?.Invoke(null);
+
+                        if (rollback != null)
+                            await rollback(null);
                     }
 
                     return res;

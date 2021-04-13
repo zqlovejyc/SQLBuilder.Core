72ff4b8d1594d7bf14a846ff5fb1541a8e245e0d
zqlovejyc <943620963@qq.com>
2021-04-13 10:56:21 +0800

    更新；

--- SQLBuilder.Core.Console/HostedService.cs
+++ SQLBuilder.Core.Console/HostedService.cs
@@ -27,7 +27,7 @@ namespace SQLBuilder.Core
 
                 try
                 {
-                    var repository = new OracleRepository(@"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.32.44.54)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=giteadev)));Persist Security Info=True;User ID=giteadev;Password=giteadev;");
+                    var repository = new OracleRepository(@"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=giteadev)));Persist Security Info=True;User ID=uid;Password=pwd;");
 
                     repository.PreCommitResultAsyncQueue.Enqueue(async repo =>
                     {

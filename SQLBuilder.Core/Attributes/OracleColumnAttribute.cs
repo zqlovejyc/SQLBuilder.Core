0432dbe0af55f5ac23511f87062959d9b100f10e
zqlovejyc <943620963@qq.com>
2021-12-10 11:01:37 +0800

    修改OracleColumnAttribute注释；

--- SQLBuilder.Core/Attributes/OracleColumnAttribute.cs
+++ SQLBuilder.Core/Attributes/OracleColumnAttribute.cs
@@ -4,7 +4,7 @@ using Oracle.ManagedDataAccess.Client;
 namespace SQLBuilder.Core.Attributes
 {
     /// <summary>
-    /// Oracle列特性
+    /// Oracle列特性，此特性仅用于Oracle特殊数据库字段类型使用，如：[OracleColumn(DbType = OracleDbType.NClob)]
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class OracleColumnAttribute : Attribute

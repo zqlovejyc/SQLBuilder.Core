17074ab696e0e2eb18534f9cefea85d632666bed
zqlovejyc <943620963@qq.com>
2022-03-24 12:20:47 +0800

    发布2.3.3版本；

--- SQLBuilder.Core/Expressions/MemberExpressionResolver.cs
+++ SQLBuilder.Core/Expressions/MemberExpressionResolver.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 /***
- * Copyright © 2018-2022, 张强 (943620963@qq.com).
+ * Copyright © 2018-2025, 张强 (943620963@qq.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

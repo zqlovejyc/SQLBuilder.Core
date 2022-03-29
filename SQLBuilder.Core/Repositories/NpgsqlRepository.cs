1be779d0795a9347e419946f46da2106e8fd3478
zqlovejyc <943620963@qq.com>
2022-03-29 17:33:46 +0800

    移除析构函数；

--- SQLBuilder.Core/Repositories/NpgsqlRepository.cs
+++ SQLBuilder.Core/Repositories/NpgsqlRepository.cs
@@ -39,11 +39,6 @@ namespace SQLBuilder.Core.Repositories
 
         #region Constructor
         /// <summary>
-        /// 析构函数
-        /// </summary>
-        ~NpgsqlRepository() => base.Dispose(false);
-
-        /// <summary>
         /// 构造函数
         /// </summary>
         public NpgsqlRepository() { }
